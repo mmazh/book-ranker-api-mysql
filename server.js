edab872146b474092122cd5e9b158a29d1b1c4b9
madeeha <m4deeh4@gmail.com>
2023-07-27 18:26:06 -0400

    Fix CORS

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const votesRoutes = require('./routes/votes.routes')
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // parse requests of content-type - application/json
 app.use(express.json());
