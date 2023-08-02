56924041fa4930c41c7f443ebc0bef8772039d02
madeeha <m4deeh4@gmail.com>
2023-08-01 20:11:20 -0400

    Update CORS

--- server.js
+++ server.js
@@ -6,13 +6,6 @@ const votesRoutes = require('./routes/votes.routes')
 
 const app = express();
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST");
-//   next();
-// });
-
 app.use(cors());
 app.options('*', cors());
 
